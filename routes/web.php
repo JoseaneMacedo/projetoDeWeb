7a5b2e08c62f37b2fc1d9d4bb1d986cbf8ac91e6
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2023-11-21 17:29:29 -0300

    Alteração teste

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//Alteração teste
 
 Route::get('/', function () {
     return view('welcome');
