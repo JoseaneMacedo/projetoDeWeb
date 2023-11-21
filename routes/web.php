0f88e605289cd264125f406f6d65d9d084bc8461
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2023-11-21 17:46:43 -0300

    Segunda alteração de teste

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//Alteração teste
+//Alteração - Segunda tentativa de teste
 
 Route::get('/', function () {
     return view('welcome');
